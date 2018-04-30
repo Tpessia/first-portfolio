5e92b257b0e23892cd0726cbc73d09e2615c1d23
Tpessia <30393840+Tpessia@users.noreply.github.com>
2018-04-30 18:11:43 -0300

    fix & show last data

---
+++ projetos/weather/show-last.php
@@ -0,0 +1,36 @@
+<?php
+    $servername = "mysql.hostinger.com.br";
+    $dbname = "u330258262_clima";
+    $username = "u330258262_admin";
+    $password = "PoZvHuYgMtIp&314159";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    
+    $sql = "
+
+        SELECT date
+        FROM 'sao_paulo'
+        ORDER BY 'ID' DESC
+        LIMIT 1
+
+    ";
+
+    $result = mysqli_query($conn, $sql);
+    
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+            echo $row["date"];
+        }
+    } else {
+        echo "0";
+    }
+    
+    mysqli_close($conn);
+?>
