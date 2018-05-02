d7184f50a4691434d207d8b21d4fdc65d1d3d18e
Tpessia <30393840+Tpessia@users.noreply.github.com>
2018-05-01 22:33:45 -0300

    11 x 30 min

--- projetos/weather/cronjob.php
+++ projetos/weather/cronjob.php
@@ -1,5 +1,5 @@
 <?php
 
-echo file_get_contents("https://www.pessia.xyz/projetos/weather/sync-data.php?qnt=15");
+echo file_get_contents("https://www.pessia.xyz/projetos/weather/sync-data.php?qnt=11");
 
 ?>
