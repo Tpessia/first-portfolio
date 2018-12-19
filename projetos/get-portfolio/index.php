9c03bf6e362f95e377c72b1ae38dc39e3c6678a6
Tpessia <thiago.pessia@hotmail.com>
2018-12-19 10:09:59 -0200

    get portfolio fix

--- projetos/get-portfolio/index.php
+++ projetos/get-portfolio/index.php
@@ -8,7 +8,7 @@
             'header' => "Authorization: bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYmVuZWRhbmllbEBnbWFpbC5jb20iLCJzdWIiOiIxMDM2NCIsImVtYWlsIjoiYmVuZWRhbmllbEBHbWFpbC5jb20iLCJnaXZlbl9uYW1lIjoiRGFuaWVsIEJlbmV2aWRlcyIsImNfaGFzaCI6ImI3NTQ3OGZiLTFjZWYtNGM4Yi05MWFlLTdhOWFjZjhmYjFlMSIsImp0aSI6ImQwYjMzYjFjLTZmMDQtNDFiYS05ODJkLTBiNWE0ZGM0NWY2NyIsImlhdCI6MTU0NTIxNDAwNCwicHJhZ21hIjoid2ViIiwibmJmIjoxNTQ1MjE0MDA0LCJleHAiOjE1NDUzMDA0MDQsImlzcyI6IktpbnZBUEkiLCJhdWQiOiIqIn0.H5fKYkD15GiUEULoMNl6N0i8RYUoe-DnhdeEFQAEOhw\r\n" . 
                 "Content-Type: application/json\r\n",
             'method' => 'POST',
-            'content' => '{"portfolioId":' + $_GET["id"] + ',"offset":0,"fetch":99999}'#http_build_query($data)
+            'content' => '{"portfolioId":' . $_GET["id"] . ',"offset":0,"fetch":99999}'#http_build_query($data)
         )
     );
     $context  = stream_context_create($options);
